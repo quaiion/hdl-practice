ca93ea0071b8702aa8888e3864ab2b8ba39db836
Korney Ivanishin <korney059@gmail.com>
2025-03-21 18:04:24 +0300

    fix inverted digit activation codes in 'counters'

--- src/verilog/counters/modules/Display/submodules/SSeg_Dig_Act.v
+++ src/verilog/counters/modules/Display/submodules/SSeg_Dig_Act.v
@@ -1,10 +1,10 @@
 module SSeg_Dig_Act ( input clk, output reg [1:0]q = 0,
                       input ce,  output wire [3:0]act );
 
-        assign act = (q == 0) ? 4'b0001 :
-                     (q == 1) ? 4'b0010 :
-                     (q == 2) ? 4'b0100 :
-                                4'b1000 ;
+        assign act = (q == 0) ? 4'b1110 :
+                     (q == 1) ? 4'b1101 :
+                     (q == 2) ? 4'b1011 :
+                                4'b0111 ;
 
         always @ (posedge clk)
                 if (ce) q <= q + 1;
